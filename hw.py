c0d9ce38f274ea61697f3f813f18ea7dcc4a05e3
M.Hashim <connectmhashim@gmail.com>
2021-03-25 13:59:33 +0500

    Added routes

--- hw.py
+++ hw.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/football', methods = ['GET'])
+def football():
+    return render_template('football.html') 
+
 if __name__ == '__main__':
     app.run(debug = True, port = 8000)
 
